2be15b78b5af6cdae3f1ce769f971c90f8f12c91
unknown <sali2ayhan@gmail.com>
2022-12-12 22:22:35 +0200

    teamPage

---
+++ src/routes/ourTeam/+page.svelte
@@ -0,0 +1,6 @@
+
+<div class="rounded-t-lg  bg-black">smclsclscls</div>
+<div class="justify-center w-full space-y-6r ">
+    <h1 class="text-center font-bold text-7xl">?</h1>
+    <p class=</p>    
+</div>
