a9d0cd9efc9c35aa788fb216b91069c2dc011d14
m10ev <mtenev@outlook.com>
2022-12-12 20:24:12 +0200

    UI

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,11 +1,8 @@
 <nav>
+    <body class="bg-white"></body>
     <div class="flex justify-center w-full">
         <a class="mx-4 text-lg hover:underline" href="/">Home</a>
         <a class="mx-4 text-lg hover:underline" href="/about">About</a>
         <a class="mx-4 text-lg hover:underline" href="/ourTeam">Our Team</a>
-    </div>
-
-    <div class="flex justify-end w-full">
-        <a class="rounded-full bg-indigo-600 px-5 py-2.5 text-sm font-semibold leading-5 text-white hover:bg-indigo-700" href="/login">Sign In</a>
     </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 
-<div class="justify-center flex w-full space-y-3">
-  <h1 class="font-bold text-7xl">Want to learn Music Theory?</h1>
-</div>
-<div class="justify-center flex w-full space-y-3">
-  <p class="text-2xl md:font-extralight">then Music Guru is the place for you.</p>
-</div>
+  <div class="">
+    <div class="justify-center flex w-full space-y-3">
+      <h1 class="font-bold text-7xl text-center">Want to learn Music Theory?</h1>
+    </div>
+    <div class="justify-center flex w-full space-y-3 sm:mt-3">
+      <p class="text-2xl">then Music Guru is the place for you.</p>
+    </div>
+  </div>
+  <div class="justify-center flex w-full mt-4">
+    <button class="text-2xl border rounded-xl border-indigo-600 bg-indigo-600 text-white bg-clip-border shadow-lg hover:shadow-indigo-600 hover:border-white hover:bg-white hover:text-black w-56 h-16">
+      Coming soon!
+    </button>
+  </div>
