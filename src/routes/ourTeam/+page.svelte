23c8311832c63c1478ddb7aab8f5b010aa70ab11
unknown <sali2ayhan@gmail.com>
2022-12-14 22:02:50 +0200

    fix

--- src/routes/ourTeam/+page.svelte
+++ src/routes/ourTeam/+page.svelte
@@ -1,2 +1,8 @@
 <h1 class="justify-center">Hello world ! </h1>
 <h1 class="justify-center">Hello world ! </h1>
+<div class=" border-2 color-grey">
+    <p> hghgkkhlhl,hll</p>
+</div>
+<div class="">
+
+</div>
