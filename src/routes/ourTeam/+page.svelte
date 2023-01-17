f6750023ffd9ed6a8fd9a6e37cf2d9ada2b91c56
unknown <sali2ayhan@gmail.com>
2023-01-17 20:46:53 +0200

    fix

--- src/routes/ourTeam/+page.svelte
+++ src/routes/ourTeam/+page.svelte
@@ -1,14 +1,34 @@
- <div class="w-screen h-screen flex justify-center items-center">
+<script>
+	import { Container } from "postcss";
 
- <div class=" border border-purple-300  h-96 w-96 rounded-md  bg-gray-200" ><p class="text-2xl text-center  ">Mihail tenev</p> </div>
+</script>
+<div class="justify-center " > <span class="justify-center text-center text-6xl text-gray-900 underline decoration-sky-500/30">Our team   </span></div> 
 
- <div class=" border border-purple-300  h-96 w-96 rounded-md bg-gray-200">
-    <p  class="text-2xl text-center">Sali ayvaz</p>
- </div>
+<div class=" justify-center">   
+
+<div class=" h-64  w-96 rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 justify-center"  >
 
 
+    <h1 class=" text-left   font-semibold font-mono text-center text-lg text-blue-400"> Mihail tenev </h1>
+    <h2  class=" text-center  text-md font-mono text-gray-400">Student</h2> 
+
+    <div class="   rounded-full    ">
+        <img class="w-24 h-24 rounded-full mx-auto justify-end " src="/html-sus1.png" alt="" width="384" height="512 ">
+        </div>
+        <p class=" font-mono text-center">Hello There my Name is Mihail<br>...</p>
+
+    
+
+</div>
+    
+<div class=" h-64  w-96 rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 " >
+<h1 class=" text-left   font-semibold font-mono text-center text-lg text-blue-400"> Sali Ayvaz </h1>
+<h2  class=" text-center  text-md font-mono text-gray-400">Student</h2> 
+<div class="   rounded-full    ">
+<img class="w-24 h-24 rounded-full mx-auto justify-end " src="/html-sus1.png" alt="" width="384" height="512 ">
+</div><p class=" font-mono text-center">Hello There my Name is Sali <br>...</p>
+</div>
 
 </div>
 
 
-  
