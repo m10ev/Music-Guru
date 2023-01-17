a7196fcaf3589df3c3941677dac8ba5d6d6e9034
unknown <sali2ayhan@gmail.com>
2023-01-17 20:48:13 +0200

    done

--- src/routes/ourTeam/+page.svelte
+++ src/routes/ourTeam/+page.svelte
@@ -2,18 +2,18 @@
 	import { Container } from "postcss";
 
 </script>
-<div class="justify-center " > <span class="justify-center text-center text-6xl text-gray-900 underline decoration-sky-500/30">Our team   </span></div> 
+<div class="justify-center w-full flex " > <span class="justify-center text-center text-6xl text-gray-900 underline decoration-sky-500/30">Our team   </span></div> 
 
-<div class=" justify-center">   
+<div class=" justify-center w-full flex">   
 
-<div class=" h-64  w-96 rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 justify-center"  >
+<div class=" h-64  w-full rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 justify-center mr-4"  >
 
 
-    <h1 class=" text-left   font-semibold font-mono text-center text-lg text-blue-400"> Mihail tenev </h1>
+    <h1 class="  font-semibold font-mono text-center text-lg text-blue-400"> Mihail tenev </h1>
     <h2  class=" text-center  text-md font-mono text-gray-400">Student</h2> 
 
     <div class="   rounded-full    ">
-        <img class="w-24 h-24 rounded-full mx-auto justify-end " src="/html-sus1.png" alt="" width="384" height="512 ">
+        <img class="w-24 h-24 rounded-full mx-auto justify-end " src="prof1.png" alt="" width="384" height="512 ">
         </div>
         <p class=" font-mono text-center">Hello There my Name is Mihail<br>...</p>
 
@@ -21,11 +21,11 @@
 
 </div>
     
-<div class=" h-64  w-96 rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 " >
-<h1 class=" text-left   font-semibold font-mono text-center text-lg text-blue-400"> Sali Ayvaz </h1>
+<div class=" h-64  w-full  rounded-lg  mt-32 bg-gray-100  border-slate-400  shadow-2xl shadow-blue-500/50 " >
+<h1 class="    font-semibold font-mono text-center text-lg text-blue-400"> Sali Ayvaz </h1>
 <h2  class=" text-center  text-md font-mono text-gray-400">Student</h2> 
 <div class="   rounded-full    ">
-<img class="w-24 h-24 rounded-full mx-auto justify-end " src="/html-sus1.png" alt="" width="384" height="512 ">
+<img class="w-24 h-24 rounded-full mx-auto justify-end " src="prof1.png" alt="" width="384" height="512 ">
 </div><p class=" font-mono text-center">Hello There my Name is Sali <br>...</p>
 </div>
 
