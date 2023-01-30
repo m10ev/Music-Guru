2a1f409440bee6299ca61f1fc860b65c8a034982
unknown <sali2ayhan@gmail.com>
2023-01-30 19:37:33 +0200

    proba

--- src/routes/lesson/lesson1/plan1/+page.svelte
+++ src/routes/lesson/lesson1/plan1/+page.svelte
@@ -1,16 +1,35 @@
 <html >
+
+
+
+<nav class=" mx-auto  p-4 bg-blue-600 rounded-full "> <h1 class="  text-3x1  font-mono hover:bg-slate-600 " >Music Guru</h1> </nav>
+
+
    <div class=" justify-center  bg-blue- rounded-lg         ">
-     <h1 class="  text-7xl text-center font-mono" >Lesson 1</h1>
+     <h1 class="  text-7xl text-center font-mono " >Lesson 1</h1>
       <h2 class="text-center  text-2xl font-mono font-semibold "><span class="text-blue-600">Notes</span>. Notes duration</h2>
     </div>
 
     <div class="justify-start flex bg-slate-    ">
-        <p class="text-2xl font-normal  font-mono" >Hello there, our friend welcome to our <span class="text-center justify-center"> first lesson.</span></p>
+        <p class="text-2xl font-normal  font-mono" >Hello there, our friend welcome to our <span class="text-center justify-center t "> first lesson.</span></p>
     </div>
     
     <div>
     <h2 class="text-2xl font-mono pt-3  text-gray-900 bg-slate-200  font-extralight   ">Today we are going to learn about <span class="text-blue-600">Notes</span>. You may have heard about <span class="text-blue-600">Notes</span>, but do you know what they are used for. We can compare the importance of <span class="text-blue-600">Notes</span> in music to that of letters in languages. How would you be able to read this if you didn’t know the letters, so let’s learn about <span class="text-blue-600">Notes</span>. Every musicians best mates.<span class="text-lime-800">Basicly and easily</span>  </h2>
     </div>
-<div class="w-1/3 h-96 flex-row      bg-slate-"  > <p>So let's see what is a note exactly</p> <img  src="notes.png" alt="" > </div>
-<div class="w-1/3 h-96  bg-slate-300"> </div>
+
+    <div class="grid grid-rows-3 grid-flow-col gap-4 mt-11 ">
+      <div class="bg-blue-500">01</div>
+      <div>02</div>
+      <div>03</div>
+      <div>04</div>
+      <div>05</div>
+      <div>06</div>
+      <div>07</div>
+      <div>08</div>
+      <div>09</div>
+    </div>
+
+
+
 </html>
