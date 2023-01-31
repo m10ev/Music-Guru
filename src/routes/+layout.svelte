0395b2b58dd719bff959dd2593e433800509afda
unknown <sali2ayhan@gmail.com>
2023-01-31 22:08:29 +0200

    plan1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.css";
     import Nav from "../components/nav.svelte"
   </script>
-  <div class="p-8 max-w-6xl mx-auto">
+  <div class="mw-auto p-8 mx-auto">
   <Nav/> 
   <slot></slot>
 </div>
--- src/routes/lesson/lesson1/plan1/+page.svelte
+++ src/routes/lesson/lesson1/plan1/+page.svelte
@@ -1,45 +1,70 @@
-<html >
+<html>
+	<nav class=" mx-auto  p-4 bg-blue  h-20 items-center  text-center rounded-full ">
+		<h1 class="  text-[27px]   font-mono ">
+			<span
+				class="text-white font-mono font-bold text-transparent bg-gradient-to-r bg-clip-text from-gray-600 to-red-400"
+				>Music Guru</span
+			>
+		</h1>
+	</nav>
+
+	<div class=" justify-center  bg-slate-300 rounded-lg         ">
+		<h1 class="  text-7xl text-center font-mono ">Lesson 1</h1>
+		<h2 class="text-center  text-2xl font-mono font-semibold ">
+			<span class="text-blue-600">Notes</span>. Notes duration
+		</h2>
+	</div>
+
+	<div
+		class="  flex-auto text-2xl text-bold font-mono rounded  bg-slate-100  hover:bg-slate- mt-3 flex-auto     "
+	>
+		Hello there our friend welcome to our first lesson. Today we are going to learn about notes. You
+		may have heard about notes, but do you know what they are used for. We can compare the
+		importance of notes in music to that of letters in languages. How would you be able to read this
+		if you didn’t know the letters, so let’s learn about notes
+	</div>
+
+	<div class="  h-[450px] w-[]  mt-16 grid grid-flow-col gap-4  flex-auto  pt   ">
+		<div class="bg-slate-100 rounded-lg text-center items-center ">
+			<span class="text-center text-2xl font-bold font-mono pt    ">Whole <span class="text-blue-600">Notes</span></span>
+		</div>
+
+		<div class="border- border-gray-500 bg-slate-100 text-center rounded-lg ">
+			<span class="text-center text-2xl font-bold  font-mono pt    ">Half <span class="text-blue-600">Notes</span></span>
+		</div>
+
+		<div class="bg-slate-100 rounded-lg text-center">
+			<span class="text-center text-2xl font-bold font-mono pt    ">Quater <span class="text-blue-600">Notes</span></span>
+		</div>
+	</div>
+
+
+  <div class="  h-[450px] w-[]  mt-16 grid grid-flow-col gap-4  flex-auto  pt-2   ">
+		<div class="bg-slate-100 rounded-lg text-center items-center ">
+			<span class="text-center text-2xl font-bold font-mono pt    ">Whole <span class="text-blue-600">Notes</span></span>
+		</div>
+
+		<div class="border- border-gray-500 bg-slate-100 text-center rounded-lg ">
+			<span class="text-center text-2xl font-bold     font-mono pt    ">Half <span class="text-blue-600">Notes</span></span>
+		</div>
+
+
+	</div>
+
+	<ul class="list-inside ...">
+    <li>5 cups chopped Porcini mushrooms</li>
+    <!-- ... -->
+  </ul>
 
 
 
-<nav class=" mx-auto  p-4 bg-blue  h-20 items-center  text-center bg-opacity-    rounded-full "> <h1 class="  text-[27px]   font-mono " ><span class="text-white font-mono font-bold text-transparent bg-gradient-to-r bg-clip-text from-gray-600 to-red-400">Music Guru</span></h1> </nav>
 
 
-   <div class=" justify-center  bg-slate-300 rounded-lg         ">
-     <h1 class="  text-7xl text-center font-mono " >Lesson 1</h1>
-      <h2 class="text-center  text-2xl font-mono font-semibold "><span class="text-blue-600">Notes</span>. Notes duration</h2>
-    </div>
-
-   <div class="  flex-auto text-2xl text-bold font-mono rounded  bg-slate-  hover:bg-slate- mt-3 divide-violet-600 flex-auto     " >Hello there our friend welcome to our first lesson. Today we are going to learn about notes. You may have heard about notes, but do you know what they are used for. We can compare the importance of notes in music to that of letters in languages. How would you be able to read this if you didn’t know the letters, so let’s learn about notes</div>
-   
-<div class="  h-[280px] w-[]  mt-16 grid grid-flow-col gap-4  flex-auto bg-blue-500   p-2   ">
-
-  <div class="bg-slate-100 rounded-lg text-center items-center ">
-    <span class="text-center text-2xl font-bold font-mono pt    ">Whole note</span>
-  </div> 
-
-  <div class="border- border-gray-500 bg-slate-100 text-center rounded-lg ">
-    <span class="text-center text-2xl font-normal     font-mono pt    ">Half note</span>
-  </div> 
-
-  <div class="bg-slate-100 rounded-lg text-center">
-    <span class="text-center text-1xl font-bold font-mono pt    ">Quater note</span>
-  </div>  
-  <div class="bg-slate-100 rounded-lg text-center">
-    <span class="text-center text-2xl font-bold font-mono pt    ">Quater note</span>
-  </div>  
-
-  <div class="bg-slate-100 rounded-lg text-center">
-    <span class="text-center text-2xl font-bold font-mono pt    ">Quater note</span>
-  </div>  
-   
 
 
- 
-  
-</div>
-   
-<div class=" hover:bg-slate-100 border-1 border-blue-200  mt-5 h-[800px]"><h2 class="text-center  text-2xl font-mono font-semibold "><span class="text-blue-600">Notes</span>. Notes duration</h2></div>
- 
+
+
+
+
 
 </html>
