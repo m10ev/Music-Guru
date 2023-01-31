b54e24304a0fb86501bef4b9a01d4bc3bf8cd6aa
unknown <sali2ayhan@gmail.com>
2023-01-31 13:19:43 +0200

    proba

--- src/routes/lesson/lesson1/plan1/+page.svelte
+++ src/routes/lesson/lesson1/plan1/+page.svelte
@@ -2,7 +2,7 @@
 
 
 
-<nav class=" mx-auto  p-4 bg-blue-600 rounded-full "> <h1 class="  text-3x1  font-mono hover:bg-slate-600 " >Music Guru</h1> </nav>
+<nav class=" mx-auto  p-4 bg-blue-500  h-20 items-center  text-center bg-opacity-20    rounded-full "> <h1 class="  text-[27px]   font-mono " ><span class="text-white font-mono font-bold text-transparent bg-gradient-to-r bg-clip-text from-gray-600 to-red-400">Music Guru</span></h1> </nav>
 
 
    <div class=" justify-center  bg-blue- rounded-lg         ">
@@ -10,25 +10,15 @@
       <h2 class="text-center  text-2xl font-mono font-semibold "><span class="text-blue-600">Notes</span>. Notes duration</h2>
     </div>
 
-    <div class="justify-start flex bg-slate-    ">
-        <p class="text-2xl font-normal  font-mono" >Hello there, our friend welcome to our <span class="text-center justify-center t "> first lesson.</span></p>
-    </div>
-    
-    <div>
-    <h2 class="text-2xl font-mono pt-3  text-gray-900 bg-slate-200  font-extralight   ">Today we are going to learn about <span class="text-blue-600">Notes</span>. You may have heard about <span class="text-blue-600">Notes</span>, but do you know what they are used for. We can compare the importance of <span class="text-blue-600">Notes</span> in music to that of letters in languages. How would you be able to read this if you didn’t know the letters, so let’s learn about <span class="text-blue-600">Notes</span>. Every musicians best mates.<span class="text-lime-800">Basicly and easily</span>  </h2>
-    </div>
-
-    <div class="grid grid-rows-3 grid-flow-col gap-4 mt-11 ">
-      <div class="bg-blue-500">01</div>
-      <div>02</div>
-      <div>03</div>
-      <div>04</div>
-      <div>05</div>
-      <div>06</div>
-      <div>07</div>
-      <div>08</div>
-      <div>09</div>
-    </div>
+   <div class="h-[120px] text-2xl text-bold font-mono rounded  bg-slate-400  hover:bg-slate-300 text-" >Hello there our friend welcome to our first lesson. Today we are going to learn about notes. You may have heard about notes, but do you know what they are used for. We can compare the importance of notes in music to that of letters in languages. How would you be able to read this if you didn’t know the letters, so let’s learn about notes</div>
+   
+<div class="h-[400px]  mt-16 grid grid-flow-col gap-6    ">
+  <div class="bg-blue-200">01</div> 
+  <div class="border- border-gray-500 ">02</div> 
+  <div class="bg-blue-200 rounded-lg">03</div> 
+  
+</div>
+   
 
 
 
