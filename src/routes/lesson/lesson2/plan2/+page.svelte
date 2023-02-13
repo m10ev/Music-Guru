831f9ca51a58b53966774ccd312ca9010997a591
Sali-Ayvaz <sali2ayhan@gmail.com>
2023-02-13 22:30:10 +0200

    start

---
+++ src/routes/lesson/lesson2/plan2/+page.svelte
@@ -0,0 +1,15 @@
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap w-full mb-20">
+        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Lesson 2.Tones</h1>
+          <div class="h-1 w-28  bg-indigo-500 rounded"></div>
+        </div>
+
+        <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-700">Tones</h1>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 mt-12 ">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+
+    
+
+</section>
