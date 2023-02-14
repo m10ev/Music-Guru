06d0cf169cc3542d2c46a20ee735b80559ed5674
Sali-Ayvaz <sali2ayhan@gmail.com>
2023-02-14 22:59:28 +0200

    lesson2

--- src/routes/lesson/lesson2/plan2/+page.svelte
+++ src/routes/lesson/lesson2/plan2/+page.svelte
@@ -1,15 +1,120 @@
 <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap w-full mb-20">
-        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Lesson 2.Tones</h1>
-          <div class="h-1 w-28  bg-indigo-500 rounded"></div>
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Lesson 2. Tones </h1><div class="h-1 w- bg-indigo-500"></div>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-md mt-4">And hello, there again our friend. We are at a important moment of our education in music- the tones. Tone has two meaning, one of them we will be looking at this lesson.   </p>
+      
+<div class="justify-start"  > <h1 class="text-3xl mt-4 text-gray-900 flex text ">What is a note </h1> <p class="text-start ">Tone is a word with two meanings. <br>- Constant sound with certain frequency of oscillation; <br>- unit of measure for distance between tones; <br> it is marked with notes.</p>
+<p class="text-2xl mt-4 text-gray-900 flex text-start"> A musical tone is characterized by it's </p> <p class="text-start">- duration <br>-pitch <br>-timbre (the unique sound of each instrument or voice) <br>-intencity (loudnes) </p>
+
+</div> 
+<h1 class="text-3xl mt-4 text-gray-900   ">Main tones written with notes </h1> <p class=" text-blue-600 text-lg"> Do (C) Re (D) Mi (E) Fa (F) Sol (G) La (A) Si (B)</p>
+ 
+  
+
+
+
+
+
+
+
+
+
+
+    </div>
+    <div class="container px-5 py-10  mx-auto">
+      <div class="flex flex-wrap -m-4">
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt=" 01" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/do.webp">
+          </a>
+          <div class="mt-4">
+           
+            <h2 class="text-gray-900 title-font text-lg font-medium">Do (C)</h2>
+           
+          </div>
         </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full ">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img  alt="ecommerce" class="object-cover object-center w-30   h-48   block" src="https://music-guru.vercel.app/re.webp">
+          </a>
+          <div class="mt-4">
+            
+            <h2 class="text-gray-900 title-font text-lg font-medium">Re (D)</h2>
+            
+          </div>
+        </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/mi.webp">
+          </a>
+          <div class="mt-4">
+            
+            <h2 class="text-gray-900 title-font text-lg font-medium">Mi (E)</h2>
+            
+          </div>
+        </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/fa.webp">
+          </a>
+          <div class="mt-4">
+            
+            <h2 class="text-gray-900 title-font text-lg font-medium">Fa (F)</h2>
+          
+          </div>
+        </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/sol.webp">
+          </a>
+          <div class="mt-4">
+            
+            <h2 class="text-gray-900 title-font text-lg font-medium">Sol (G)</h2>
+           
+          </div>
+        </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/la.webp">
+          </a>
+          <div class="mt-4">
+            
+            <h2 class="text-gray-900 title-font text-lg font-medium">La (A)</h2>
+       
+          </div>
+        </div>
+        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <a class="block relative h-48 rounded overflow-hidden">
+            <img alt="ecommerce" class="object-cover object-center w-30 h-48 block" src="https://music-guru.vercel.app/si.webp">
+          </a>
+          <div class="mt-4 justify-end" >
+           
+            <h2 class="text-gray-900 title-font text-lg font-medium">Si (B/H)</h2>
+        
+          </div>
+        </div>
+     
+      </div>
+    </div>
+        
+      
+    <p class="font-bold">! 
+      In the language of music theory, a step is the distance between tones. A half step, or semitone, is the smallest interval between tones. Tones such as E and F or B and C, are a half-step apart. Two half steps equal one whole step the tones G and A, A and B, C and E are one whole step apart ! </p>
+   <p>
+  Do--Re--Mi-Fa--Sol--La--Si-Do
+  * - semitone
+   </p>
+   <div class="h-1 w-full bg-indigo-500 mt-6"></div>
+   <div>
+       <p class="text-center mt-3 font-semibold ">New Words</p>
+   <p> Tone (sound) - Constant sound with certain frequency of oscillation;<br>
+    tone (distance) - distance between tones,notes;<br>
+    timbre- the unique sound of each instrument or voice;
+   </p>
+       
 
-        <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-700">Tones</h1>
-            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500 mt-12 ">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
 
-    
+
 
 </section>
